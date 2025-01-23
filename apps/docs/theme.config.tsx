d2bfa7cf984871af4f5ac706090b5b5c02cf1cad
Rabi (Reserva) <yeytaken.js@gmail.com>
2025-01-23 01:20:41 +0000

    Feat: system translator add.

--- apps/docs/theme.config.tsx
+++ apps/docs/theme.config.tsx
@@ -2,6 +2,10 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
+  i18n: [
+    { locale: 'en', text: 'English' },
+    { locale: 'pt', text: 'Português' },
+  ],
   banner: {
     key: 'discord',
     text: (

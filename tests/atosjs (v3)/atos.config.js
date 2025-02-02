5cdbd746e848580ef551fa97451e8e9a03855cb8
Rabi (Reserva) <yeytaken.js@gmail.com>
2025-02-02 02:47:44 +0000

    Feat: atos CLI test

--- tests/atosjs (v3)/atos.config.js
+++ tests/atosjs (v3)/atos.config.js
@@ -1,13 +1,13 @@
 import { GiftManager, TimeFormat } from '../../packages/atosjs/dist/index.js';
 import dotenv from 'dotenv';
-
 dotenv.config();
 
 export const time = new TimeFormat();
 
 export const gift = new GiftManager({
-    mongodb: {
-        connect: process.env.DB_URI,
-        dbName: 'gift_db',
-    }
+  mongodb: {
+    connect: process.env.DB_URI,
+    dbName: 'gifts',
+  },
 });
+

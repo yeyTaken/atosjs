1ccad2570546afeff76a982bc94688552813faf3
Rabi (Reserva) <yeytaken.js@gmail.com>
2025-02-16 18:02:20 +0000

    Feat: edit /app/_meta.js

--- apps/website/atosjs/app/_meta.js
+++ apps/website/atosjs/app/_meta.js
@@ -2,26 +2,33 @@
 import { FaBook } from 'react-icons/fa';
 // import { FaNewspaper } from "react-icons/fa6";
 
+const docs = (
+  <span style={{ display: 'flex', alignItems: 'center' }}>
+    <FaBook style={{ marginRight: '0.5em' }} />
+    Docs
+  </span>
+);
+/*
+const blog = (
+  <span style={{ display: 'flex', alignItems: 'center' }}>
+    <FaNewspaper style={{ marginRight: '0.5em' }} />
+    Blog
+  </span>
+
+);
+*/
+
+
 export default {
   index: {
     display: 'hidden'
   },
   docs: {
     type: 'page',
-    title: (
-      <span style={{ display: 'flex', alignItems: 'center' }}>
-        <FaBook style={{ marginRight: '0.5em' }} />
-        Docs
-      </span>
-    ),
+    title: docs,
   },
   /*blog: {
     type: 'page',
-    title: (
-      <span style={{ display: 'flex', alignItems: 'center' }}>
-        <FaNewspaper style={{ marginRight: '0.5em' }} />
-        Blog
-      </span>
-    )
+    title: blog,
   } */
 }

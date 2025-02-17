e6505a1a85d9cfacff7708c87a868112fab9a61d
Rabi <99534446+yeyTaken@users.noreply.github.com>
2025-02-17 11:15:22 -0300

    Update layout.jsx

--- apps/website/atosjs/app/layout.jsx
+++ apps/website/atosjs/app/layout.jsx
@@ -3,8 +3,7 @@ import { Footer, Layout, Navbar } from 'nextra-theme-docs';
 import { Head } from 'nextra/components';
 import { getPageMap } from 'nextra/page-map';
 
-// import './global.css'
-import 'nextra-theme-docs/style.css';
+import './globals.css';
 
 export const metadata = {
   metadataBase: new URL('https://nextra.site'),

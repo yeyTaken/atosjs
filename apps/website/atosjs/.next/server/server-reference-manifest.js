a24f4c5261199c40e825ca3156e8114be3da8e59
Rabi <yeytaken.ofc@gmail.com>
2025-02-19 15:41:17 +0000

    Feat: geral edit

--- apps/website/atosjs/.next/server/server-reference-manifest.js
+++ apps/website/atosjs/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"wwRIpVZQ/FvgQW2n4+dPXJ5D5rGFksQambtKVintS7Y=\"\n}"

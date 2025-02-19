f50b44e9a440f0d426d55c46c07f9d1af64d5a86
Rabi (Reserva) <yeytaken.js@gmail.com>
2025-02-19 14:11:15 +0000

    Feat: news export in AtosJS pqckages

--- apps/website/atosjs/.next/server/server-reference-manifest.js
+++ apps/website/atosjs/.next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"64rGx/E8iW9RfdhpmGbkXsRnC9qcjJprbCHvZ3YTYv4=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"

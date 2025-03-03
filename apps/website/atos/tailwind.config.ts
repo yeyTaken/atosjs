b31e2c6a543e968c992466e06cfb62ead3c15ba5
Rabi <99534446+yeyTaken@users.noreply.github.com>
2025-03-03 06:36:55 -0300

    Atualizar o tailwind.config.ts

--- apps/website/atos/tailwind.config.ts
+++ apps/website/atos/tailwind.config.ts
@@ -1,3 +1,13 @@
 import type { Config } from 'tailwindcss'
 
 export default {} satisfies Config
+
+import { heroui } from '@heroui/theme';
+import type { Config } from "tailwindcss";
+
+export default {
+  content: [
+     "./node_modules/@heroui/theme/dist/components/(snippet|button|ripple|spinner|popover).js"
+  ],
+  plugins: [heroui()],
+} satisfies Config;

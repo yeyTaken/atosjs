ea74ca6ea4e72f23159cc67185654042757ebcaf
Rabi <99534446+yeyTaken@users.noreply.github.com>
2025-05-02 11:47:48 -0300

    AtosJS@1.0.0

--- apps/website/atos/src/components/en/About/devs.tsx
+++ apps/website/atos/src/components/en/About/devs.tsx
@@ -8,7 +8,7 @@ export default function Developers() {
       <section className="container mx-auto py-12 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
         <Card
           image="https://i.imgur.com/crKMa5j.jpg"
-          bannerImage="https://images.wallpapersden.com/image/download/digital-roronoa-zoro-minimal-one-piece-art_bmdna2aUmZqaraWkpJRmbmdsrWZlbWU.jpg"
+          bannerImage="https://truth.bahamut.com.tw/s01/201907/bac54e76fc4ff5b3691357c90c950bbb.JPG"
           name="Israel R. Jatobá"
           description="I’m Rabi, a beginner in the programming world, and I build projects in my spare time."
           tags={[1, 2, 7]}
